eea765e4f322d3c8a25bcd5c195fbfd49896bb5c
Fazlay-Elahi-Rafi <fazlayrafi2001@gmail.com>
2025-12-23 21:52:04 +0600

    Update changes

--- frontend/src/hooks/useAuthUser.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { RootState } from "store/index";
-
-const useAuthUser = () => {
-  const data = useSelector((state: RootState) => state.authUser);
-  return data;
-};
-
-export default useAuthUser;
